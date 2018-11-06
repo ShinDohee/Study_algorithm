71643876f978baef7b39a8a52fb2eaf4195ff72b
doheeShin <intei0909@gmail.com>
2018-11-06 14:41:37 +0900

    Day of the Programmer_11.06

--- study_code/src/hackerrank/Solution_DayoftheProgrammer.java
+++ study_code/src/hackerrank/Solution_DayoftheProgrammer.java
@@ -5,28 +5,40 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Solution_DayoftheProgrammer {
     // Complete the dayOfProgrammer function below.
     static String dayOfProgrammer(int year) {
-        return "";
+        String date = "";
+        if(year < 1918) {                                                   
+            date += (year % 4 == 0) ? "12.09." + year : "13.09." + year;
+        } else if(year == 1918) {                                           
+            date += "26.09." + year;
+        } else {                                                          
+            date += ((year % 400 == 0) ||                               
+            (year % 4 == 0 && year % 100 != 0)) ? "12.09." + year : "13.09." + year;
+        }
+        return date;
 
     }
+    
+
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(
-                System.getenv("OUTPUT_PATH")));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(
+//                System.getenv("OUTPUT_PATH")));
 
         int year = Integer.parseInt(bufferedReader.readLine().trim());
 
         String result = dayOfProgrammer(year);
-
-        bufferedWriter.write(result);
-        bufferedWriter.newLine();
+        System.out.println(result);
+//        bufferedWriter.write(result);
+//        bufferedWriter.newLine();
 
         bufferedReader.close();
-        bufferedWriter.close();
+//        bufferedWriter.close();
     }
 }
