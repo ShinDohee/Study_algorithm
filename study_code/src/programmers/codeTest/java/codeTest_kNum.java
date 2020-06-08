8c47f3bc5667cf53a826dcec30ea4a4cd2ceaa5d
ShinDohee <intei0909@gmail.com>
2020-06-08 16:39:26 +0900

    2020.06.08 프로그래머스 코드테스트 k 번째 수 - java version

--- study_code/src/programmers/codeTest/java/codeTest_kNum.java
+++ study_code/src/programmers/codeTest/java/codeTest_kNum.java
@@ -1,5 +1,37 @@
 package programmers.codeTest.java;
 
-public class codeTest_kNum {
+import java.util.Arrays;
 
+public class codeTest_kNum {
+	/**
+	 * 	
+	array						commands							return
+	[1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+
+	 */
+
+		
+		public static void main(String[] args) {
+			int[] array = {1, 5, 2, 6, 3, 7, 4};
+			int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};	
+			
+			solution(array , commands).toString();
+		}
+
+		public static int[] solution(int[] array, int[][] commands) {
+			int[] answer = new int[commands.length] ;
+			int [] temp =  new int[commands.length] ;
+			System.out.print("[");
+			for (int i = 0; i < commands.length; i++) {
+				if(i !=0) System.out.print(",");
+				temp= Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+				Arrays.sort(temp);//오름차순			
+				answer[i]= temp[commands[i][2]-1];
+				System.out.print(answer[i]);
+			}
+			System.out.print("]");
+			
+			return answer;
+		}
+	
 }
