af8edd8468a14ed216552e3e48d1d7eb7d8efdc9
ShinDohee <intei0909@gmail.com>
2020-06-08 13:10:28 +0900

    2020.06.08

--- study_code/src/programmers/codeTest/javasript/codeTest_Knum.js
+++ study_code/src/programmers/codeTest/javasript/codeTest_Knum.js
@@ -1,3 +1,21 @@
 /**
  * 
  */
+
+function solution(array, commands) {
+    var answer = [];
+    var emptyArr = [];
+    
+    for(var i=0; i<commands.length; i++){
+    	var vsArray = array;
+        
+    	emptyArr= vsArray.slice(commands[i][0]-1,commands[i][1]);
+    	emptyArr.sort(function (a,b){
+    		return a-b;
+    	});
+
+    	answer[i] = emptyArr[commands[i][2]-1];
+    }
+    
+    return answer;
+}
