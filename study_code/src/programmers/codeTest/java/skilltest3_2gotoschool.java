85629e9950490a2759a4716d8ee3ba7f14addc13
doheeshin <intei0909@gmail.com>
2020-07-10 09:23:28 +0900

    #programmers 등굣길

--- study_code/src/programmers/codeTest/java/skilltest3_2gotoschool.java
+++ study_code/src/programmers/codeTest/java/skilltest3_2gotoschool.java
@@ -1,7 +1,7 @@
 package programmers.codeTest.java;
 
 public class skilltest3_2gotoschool {
-    public int solution(int m, int n, int[][] puddles) {
+    public static int solution(int m, int n, int[][] puddles) {
         int answer = 0;
         
         int[][] d = new int[n+1][m+1];
@@ -30,35 +30,8 @@ public class skilltest3_2gotoschool {
         
         return answer;
     }
-    public static int solution1(int m, int n, int[][] puddles) {
-        /* øıµ¢¿Ã √ ±‚»≠ */
-        boolean[][] isPuddles = new boolean[n][m];
-        int[][] map = new int[n][m];
-        for (int i = 0; i < puddles.length; i++)
-            isPuddles[puddles[i][1] - 1][puddles[i][0] - 1] = true;
-         
-        boolean flag = true;
-        for(int i = 0; i < m; i++) { 
-            if(isPuddles[0][i]) flag = false;
-            if(flag) map[0][i] = 1;
-        }
-         
-        flag = true;
-        for(int i = 0; i < n; i++) {
-            if(isPuddles[i][0]) flag = false;
-            if(flag) map[i][0] = 1;
-        }
-         
-        for(int i = 1; i < n; i++) 
-            for(int j = 1; j < m; j++) 
-                if(!isPuddles[i][j]) 
-                    map[i][j] = (map[i][j - 1] + map[i - 1][j]) % 1000000007;
- 
-        return map[n - 1][m - 1];
-    }
-     
     public static void main(String[] args) {
         int[][] puddles = {{2, 2}, {3, 2}};
-        System.out.println(solution1(4, 3, puddles));
+        System.out.println(solution(4, 3, puddles));
     }
 }
