f210d54b7c4f4782cf7c5c4e4f111e261dc6b477
ShinDohee <intei0909@gmail.com>
2020-11-13 10:22:21 +0900

    2020.11.13 Day 3: Intro to Conditional Statements

---
+++ study_code/src/hackerrank/Solution_Day3_Intro.java
@@ -0,0 +1,27 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Solution_Day3_Intro {
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int N = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		String print_str="";
+		if (N%2 ==0) {
+			if(2<N&&N<5) {
+				print_str="Not Weird";
+			}else if(6<N&&N<=20) {
+				print_str="Weird";				
+			}else if(20<N) {
+				print_str="Not Weird";
+			}
+		}else {
+			print_str="Weird";				
+			
+		}
+		System.out.println(print_str);
+		scanner.close();
+	}
+}
