5a3e271d73514a81a8277b9d52e4d53645d7b22f
ShinDohee <intei0909@gmail.com>
2024-03-25 23:35:45 +0900

    24.01.03 day 21

--- study_code/src/programmers/bagic_training/Level00/Solution00_sequenceNum.java
+++ study_code/src/programmers/bagic_training/Level00/Solution00_sequenceNum.java
@@ -2,6 +2,8 @@ package programmers.bagic_training.Level00;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
 public class Solution00_sequenceNum {
     /**
@@ -62,7 +64,54 @@ public class Solution00_sequenceNum {
 
         return arr.stream().mapToInt(Integer::intValue).toArray();
     }
+
+    /**
+     *
+     전국 대회 선발 고사
+     제출 내역
+     문제 설명
+     0번부터 n - 1번까지 n명의 학생 중 3명을 선발하는 전국 대회 선발 고사를 보았습니다. 등수가 높은 3명을 선발해야 하지만, 개인 사정으로 전국 대회에 참여하지 못하는 학생들이 있어 참여가 가능한 학생 중 등수가 높은 3명을 선발하기로 했습니다.
+
+     각 학생들의 선발 고사 등수를 담은 정수 배열 rank와 전국 대회 참여 가능 여부가 담긴 boolean 배열 attendance가 매개변수로 주어집니다.
+     전국 대회에 선발된 학생 번호들을 등수가 높은 순서대로 각각 a, b, c번이라고 할 때
+     10000 × a + 100 × b + c를 return 하는 solution 함수를 작성해 주세요.
+     [3, 7, 2, 5, 4, 6, 1]	[false, true, true, true, true, false, false]	20403
+     * @param rank
+     * @param attendance
+     * @return
+     */
+    public static int solution_rank(int[] rank, boolean[] attendance) {
+        PriorityQueue<Integer> pq = IntStream.range(0, rank.length)
+                .filter(i -> attendance[i])
+                .mapToObj(i -> rank[i])
+                .collect(PriorityQueue::new, PriorityQueue::offer, PriorityQueue::addAll);
+
+        int[] selected = IntStream.range(0, 3)
+                .mapToObj(i -> pq.poll())
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        int a = findIndex(rank, selected[0]);
+        int b = findIndex(rank, selected[1]);
+        int c = findIndex(rank, selected[2]);
+
+        return a * 10000 + b * 100 + c;
+    }
+
+    private static int findIndex(int[] array, int targetValue) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == targetValue) {
+                return i;
+            }
+        }
+        return -1; // 찾고자 하는 값이 배열에 없는 경우
+    }
+
     public static void main(String[] args) {
+      int [] num = {3, 7, 2, 5, 4, 6, 1};
+      boolean [] attention = {false, true, true, true, true, false, false};
+
+        solution_rank(num, attention);
 
     }
 }
